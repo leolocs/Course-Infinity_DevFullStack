4a9a0713b4537dca3f53ba0ab6730d2176350ac5
leolocs <54726722+leolocs@users.noreply.github.com>
2023-10-03 19:03:57 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+# nome = "Leonardo"
+# a = 25
+# altura = 1.76
+# matriculado = True
+
+
+
+#Operadores Aritméticos
+# num1 = 5
+# num2 = 2
+
+# print(num1+num2)
+# print(num1-num2)
+# print(num1*num2)
+# print(num1/num2)
+# print(num1//num2)
+# print(num1**num2)
+
+#Operadores Relacionais
+# n1=9
+# n2=4
+
+# print (n1==n2)
+# print (n1!=n2)
+# print (n1>n2)
+# print (n1<n2)
+# print (n1>=n2)
+
+# nome_do_aluno = input("Digite seu nome:")
+# print("bem-vindo", nome_do_aluno,"!")
+# print(f"Bem vindo {nome_do_aluno}!")
+
+#Print type
+
---
+++ questions_1-20.py
@@ -0,0 +1,58 @@
+#Questão 2
+# nome = str(input("Digite seu nome: "))
+# print(f"Olá {nome}, seja bem vindo")
+
+#Questão 3
+# idade = int(input("Digite sua idade: "))
+# print(f"Sua idade é: {idade}")
+
+#Questão 4
+# nome = str(input("Digite seu nome:"))
+# idade = int(input("Qual sua idade ? "))
+# print (f"""Seu nome é:{nome}
+# Sua idade é: {idade}""")
+
+#Questão 5
+# print(nome, idade)
+
+# Questão 6
+# num1 = int(input("Digite um numero: "))
+# num2 = int(input("Digite outro número: "))
+# print(num1+num2)
+
+# Questão 7
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite outro número: ")) 
+# print(num1-num2)
+
+# Questão 8
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite outro número: "))
+# print(num1*num2)
+
+# Questão 9
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite outro número: "))
+# print(num1*num2)
+
+# Questão 10
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite outro número: "))
+# print(num1%num2)
+
+# Questão 11
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite outro número: "))
+# print(num1//num2)
+
+# Questão 12
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite outro número: "))
+# print(num1**num2)
+
+# Questão 13
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite outro número: "))
+print(num1!=num2)
+
+# Questão 14
