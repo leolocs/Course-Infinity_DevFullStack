c57cd9f3f7bd6673232a54b68a164818852f5ffe
Leonardo Souza <54726722+leolocs@users.noreply.github.com>
2023-10-30 23:45:34 -0300

    Add files via upload

---
+++ programming_logic/aula1-algoritmos_e_variaveis/Log. Programacao_Q1-Q20.py
@@ -0,0 +1,111 @@
+#Questão 1
+#Crie um algoritmo que exiba na tela (terminal) a mensagem "Olá Mundo".
+#print('Helo Word')
+
+#Questão 2
+#Crie um algoritmo que exiba na tela (terminal) seu nome. Por exemplo: "Olá,João Paulo."
+# nome = 'Leonardo'
+# print(f"Olá {nome}, seja bem vindo")
+
+#Questão 3
+# Crie um algoritmo que exiba na tela (terminal) sua idade. Por exemplo: 17.
+# idade = int(input("Digite sua idade: "))
+# print(f"Sua idade é: {idade}")
+
+#Questão 4
+# Crie um algoritmo que exiba na tela (terminal) (em linhas separadas) seu nome e sua idade.
+# nome = str(input("Digite seu nome:"))
+# idade = int(input("Qual sua idade ? "))
+# print (f"""Seu nome é:{nome}
+# Sua idade é: {idade}""")
+
+#Questão 5
+# Crie um algoritmo que exiba na tela (terminal) (na mesma linha) seu nome e sua idade.
+# nome = 'Leonardo'
+# idade = 25
+# print(nome, idade)
+
+# Questão 6
+# Crie um algoritmo que exiba na tela (terminal) a soma de dois números inteiros.
+# num1 = int(input("Digite um numero: "))
+# num2 = int(input("Digite outro número: "))
+# print(num1+num2)
+
+# Questão 7
+# Crie um algoritmo que exiba na tela (terminal) a subtração de dois números inteiros.
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite outro número: ")) 
+# print(num1-num2)
+
+# Questão 8
+# Crie um algoritmo que exiba na tela (terminal) a multiplicação de dois números inteiros.
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite outro número: "))
+# print(num1*num2)
+
+# Questão 9
+# Crie um algoritmo que exiba na tela (terminal) a divisão de dois números inteiros.
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite outro número: "))
+# print(num1*num2)
+
+# Questão 10
+# Crie um algoritmo que exiba na tela (terminal) o resto da divisão de dois números inteiros.
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite outro número: "))
+# print(num1%num2)
+
+# Questão 11
+# Crie um algoritmo que exiba na tela (terminal) apenas a parte inteira de uma divisão de dois números inteiros.
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite outro número: "))
+# print(num1//num2)
+
+# Questão 12
+# Crie um algoritmo que exiba na tela (terminal) o quadrado de um número inteiro.
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite outro número: "))
+# print(num1**num2)
+
+# Questão 13
+# Crie um algoritmo que exiba na tela (terminal) o valor lógico de uma variável qualquer.
+# num1 = int(input("Digite um numero: "))
+# num2 = int(input("Digite outro número: "))
+# print(num1!=num2)
+
+# Questão 14
+# Crie um algoritmo que exiba na tela (terminal) o tipo de uma variável cujo valor atribuído é "jpaulo@infinityschoool.com.br".
+# email = 'jpaulo@infinityschool.com.br'
+# print(type(email))
+
+# Questão 15 
+# Crie um algoritmo que exiba na tela (terminal) o tipo de uma variável cujo valor atribuído é "c".
+# letra = "c"
+# print(type(letra))
+
+# Questão 16 
+# Crie um algoritmo que exiba na tela (terminal) o tipo de uma variável cujo valor atribuído é 12.
+# num = 12
+# print(type(num))
+
+# Questão 17
+# Crie um algoritmo que exiba na tela (terminal) o tipo de uma variável cujo valor atribuído é 1.87.
+# numf = 1.87
+# print(type(numf))
+
+# Questão 18
+# Crie um algoritmo que exiba na tela (terminal) o tipo de uma variável cujo valor atribuído é False.
+# solteiro = False
+# print(type(solteiro))
+
+# Questão 19
+# Crie um algoritmo que concatene duas strings ao ser executado. Exiba a string concatenada na tela (terminal).
+# str1 = 'Leonardo'
+# str2 = 'Casado'
+# print(f"{str1} é {str2}" )
+
+# Questão 19
+# Crie um algoritmo que concatene dois numeros inteiros ao ser executado. Exiba os números concatenados na tela (terminal).
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+print(f"A soma de {num1} + {num2} é: {num1+num2}")
---
+++ programming_logic/aula2-estrutura_de_condicao/prova_condicional_if_elif_else.py
@@ -0,0 +1,13 @@
+# PROVA DE CONDICIONAL:  Escreva um programa em python que pergunte ao usuário a velocidade de um carro. 
+#Caso ultrapasse 80 km/h, exiba uma mensagem dizendo que o usuário foi multado. 
+#Nesse caso, exiba o valor da multa, cobrando R$20,00 por cada km que exceder 80 km/h.
+
+while True:
+    velocidade = float(input("Qual a velocidade? "))
+    multa = 0
+    if velocidade > 80:
+        multa = (velocidade - 80) * 20
+        print(f"Você foi multado por excesso de velocidade, multa de R${multa}")
+    else:
+        print("Continue dirigindo em segurança!")
+        break
