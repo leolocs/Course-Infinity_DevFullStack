639ca3d061ef0e02896485cb9f8d1ed916dfacfa
Leonardo Souza <54726722+leolocs@users.noreply.github.com>
2024-01-20 12:26:58 -0300

    Delete python/aula5_functions_python directory

--- python/aula5_functions_python/Function_transforma_maiscula.py
+++
@@ -1,7 +0,0 @@
-#Q1.Faça um programa que recebe uma palavra qualquer e retorne essa mesma palavra toda em maiusculo
-def transf_maiuscula(texto:str):
-    return texto.upper()
-
-palavra = str(input("Digite uma palavra: "))
-print(transf_maiuscula(texto=palavra))
-
