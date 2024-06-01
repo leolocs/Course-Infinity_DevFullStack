6bb1ab80059ea4e71305ceab3c045e376fc446bd
Leonardo Souza <54726722+leolocs@users.noreply.github.com>
2024-06-01 17:33:26 -0300

    Delete Aula1 - Javascript, Introdução ao Javascript directory

--- Aula1 - Javascript, Introdução ao Javascript/atv_3-(calcula_idade).js
+++
@@ -1,7 +0,0 @@
-// Peça ao usuário para digitar sua idade usando o prompt e, em seguida, calcule e exiba o ano de nascimento com uma frase personalizada usando o console.
-
-const idade = Number(prompt("Digite sua idade: "))
-const data_atual = new Date().getFullYear()
-const ano_nasc = data_atual-idade
-
-alert(`Seu ano de nascimento é: ${ano_nasc}`)
--- Aula1 - Javascript, Introdução ao Javascript/atv_6-(desconto_produto).js
+++
@@ -1,6 +0,0 @@
-//Peça ao usuário que insira um preço de um produto usando o prompt. Em seguida, aplique um desconto de 10% a esse preço e exiba o preço com desconto no console.
-
-const preco = Number(prompt("Qual o preço do produto? "))
-const valor_c_desconto = preco - preco * 0.10
-
-alert(`O preço do produto com desconto é: ${valor_c_desconto}`)
