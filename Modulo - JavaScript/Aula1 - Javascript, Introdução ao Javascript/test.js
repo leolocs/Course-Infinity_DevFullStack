1e4b593712960d7295460fa8e7b319eff951a9a5
Leonardo Souza <54726722+leolocs@users.noreply.github.com>
2024-06-08 17:31:43 -0300

    Add files via upload

---
+++ Modulo - JavaScript/Aula1 - Javascript, Introdução ao Javascript/test.js
@@ -0,0 +1,21 @@
+/*document.write("olá mundo <br>")
+const nome = prompt("Digite aqui seu nome: ")
+const idade = prompt("Digite sua idade: ")
+const cpf = prompt("Digite seu CPF: ")
+
+document.write("Bem vindo ",nome, ", seu cadastro foi um sucesso, sua idade é ",idade,", seu CPF é:", cpf)
+
+document.write(`Bem vindo(a) ${nome} seu cadastro foi um sucesso <br> Sua idade é ${idade} <br> CPF: ${cpf}` )
+
+*/
+
+let idade = 28
+idade = idade + 1
+idade += 1
+idade ++
+
+let salario = 1500
+
+salario = salario + 200
+salario += 200
+
