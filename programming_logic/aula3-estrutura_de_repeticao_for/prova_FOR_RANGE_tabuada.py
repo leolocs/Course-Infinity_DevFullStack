233043f6a8f1b531d662e7c4e924c38547686ba2
Leonardo Souza <54726722+leolocs@users.noreply.github.com>
2024-06-08 15:43:52 -0300

    Delete programming_logic directory

--- programming_logic/aula3-estrutura_de_repeticao_for/prova_FOR_RANGE_tabuada.py
+++
@@ -1,5 +0,0 @@
-#region - 2.Escreva um programa que solicite ao usuário um número inteiro e imprima a tabuada desse número, de 1 a 10.
-num = int(input("Digite um número: "))
-for n in range(1,11):
-    print(f"{num} x {n} = {num*n}")
-#endregion
