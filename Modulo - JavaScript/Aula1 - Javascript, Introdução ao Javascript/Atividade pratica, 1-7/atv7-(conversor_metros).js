ebe4727b907e34cbb1b7132ccbc70cedd557dc15
Leonardo Souza <54726722+leolocs@users.noreply.github.com>
2024-06-08 17:31:10 -0300

    Delete Modulo - JavaScript/Aula1 - Javascript, Introdução ao Javascript directory

--- Modulo - JavaScript/Aula1 - Javascript, Introdução ao Javascript/Atividade pratica, 1-7/atv7-(conversor_metros).js
+++
@@ -1,7 +0,0 @@
-// Atividade pratica - 7: Solicite ao usuário que insira um valor em metros usando o prompt e, em seguida, converta esse valor para centímetros (1 metro = 100 centímetros) e
-// exiba o resultado no console.
-
-const metros = Number(prompt("Digite um valor em metros: "))
-const centimetros = metros*100
-
-alert(`O valor digitado convertido para centimetros é: ${centimetros}`)
