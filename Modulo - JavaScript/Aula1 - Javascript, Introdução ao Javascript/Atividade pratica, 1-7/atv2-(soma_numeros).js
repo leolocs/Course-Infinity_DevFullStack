b60a534cd917c8e99138b10268a006a302260264
Leonardo Souza <54726722+leolocs@users.noreply.github.com>
2024-06-08 15:41:06 -0300

    Add files via upload

---
+++ Modulo - JavaScript/Aula1 - Javascript, Introdução ao Javascript/Atividade pratica, 1-7/atv2-(soma_numeros).js
@@ -0,0 +1,8 @@
+// Solicite ao usuário que insira dois números usando o prompt, some esses números e exiba o resultado usando o console.
+
+const num1 = Number(prompt("Digite um número: "))
+const num2 = Number(prompt("Digite um segundo numero: "))
+const soma = num1+num2
+
+//document.write(`A soma dos números enviados é: ${num1+num2}`)
+alert(`A soma dos números informados é: ${soma}`)
