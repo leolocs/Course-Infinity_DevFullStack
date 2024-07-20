78b809e963a68d529338135b403e5706107c03ef
Leonardo Souza <54726722+leolocs@users.noreply.github.com>
2024-07-20 16:06:53 -0300

    Update script.js

--- Modulo - JavaScript/Aula1 - Javascript, Introdução ao Javascript/Prova Aula1 - Javascript/script.js
+++ Modulo - JavaScript/Aula1 - Javascript, Introdução ao Javascript/Prova Aula1 - Javascript/script.js
@@ -6,9 +6,13 @@
 // Lembre-se de declarar valores para os pesos e para as notas.
 
 const n1 = Number(prompt("Digite uma nota: "))
+const p1 = Number(prompt("Digite peso da primeira nota: "))
 const n2 = Number(prompt("Digite uma nota: "))
+const p2 = Number(prompt("Digite peso da segunda nota: "))
 const n3 = Number(prompt("Digite uma nota: "))
-soma = n1+n2+n3
-media = soma/3
+const p3 = Number(prompt("Digite peso da terceira nota: "))
+
+soma_peso_notas = p1+p2+p3
+media = (n1*p1 + n2*p2 + n3*p3) / soma_peso_notas
 
 console.log(`A sua média ponderada é: ${media.toFixed(0)}`)
