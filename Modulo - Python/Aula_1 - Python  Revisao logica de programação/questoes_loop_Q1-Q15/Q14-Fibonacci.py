d8e11008890866e5029a9c39d8e7ae9ab8d7bc40
Leonardo Souza <54726722+leolocs@users.noreply.github.com>
2024-07-27 19:28:42 -0300

    Update Q14-Fibonacci.py

--- Modulo - Python/Aula_1 - Python  Revisao logica de programação/questoes_loop_Q1-Q15/Q14-Fibonacci.py
+++ Modulo - Python/Aula_1 - Python  Revisao logica de programação/questoes_loop_Q1-Q15/Q14-Fibonacci.py
@@ -1,1 +1,14 @@
-#Crie um programa que gere e imprima os primeiros 10 termos da sequência de Fibonacci
+#Crie um programa que gere a sequência de Fibonacci até o número digitado pelo usuário
+# Inicializa os dois primeiros termos da sequência
+termo1 = 0
+termo2 = 1
+
+# Exibe os primeiros 10 termos da sequência de Fibonacci
+while True:
+    num = int(input("\n Digite o número que deseja saber a sequencia de fibonnaci: "))
+    for n in range(15):
+        print(termo1, end=" ")
+        proximo_termo = termo1 + termo2
+        termo1, termo2 = termo2, proximo_termo
+    break
+    
